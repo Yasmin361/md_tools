164d66085796b1ad6856ff6412ea08a5ef1aac15
Yasmin <45427466+Yasmin361@users.noreply.github.com>
2020-07-10 15:21:25 +0200

    Update trjcat_tools.py
    in-comment list comprehension expression of part-list for trjcatspan function

--- gromacs_tools/trjcat_tools.py
+++ gromacs_tools/trjcat_tools.py
@@ -7,6 +7,7 @@ def trjcatspan(x,y):
     for i in select_parts:
         partnumber = "step7_production.part"+str(i).zfill(4)+".xtc"
         part_list.append(partnumber)
+    #part_list = ["step7_production.part"+str(i).zfill(4)+".xtc" for i in select_parts if i != None]
     return "gmx trjcat -f "+" ".join(part_list)+" -o step7_production.cat"+str(first_part)+"_"+str(last_part)+".xtc"
 
 #print(trjcatspan(75, 83))
@@ -17,8 +18,8 @@ def ndx_groups(x, y):
     index_list = []
     for i in sequence:
         index_list.append(str(i))
-    return " | ".join(index_list)
 
+    return " | ".join(index_list)
 
 print(ndx_groups(321, 335))
 
