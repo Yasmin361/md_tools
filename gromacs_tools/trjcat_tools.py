5f80733542b5ddcc1ca3e9448d2f802aecc2766f
Yasmin Aydin <yasmin.aydin@ymail.com>
2020-11-20 15:43:28 +0100

    added new multigroup function to yield evenly spaced intervals of indices

--- gromacs_tools/trjcat_tools.py
+++ gromacs_tools/trjcat_tools.py
@@ -13,6 +13,9 @@ def trjcatspan(x,y):
 print(trjcatspan(75, 80))
 
 
+
+#T187 - H198 + T221-K230 + T261-T267+ V328-D342
+
 #this generates index group sequence for merging operation in gmx make_ndx
 def ndx_groups(x, y):
     sequence = range(x, y+1)
@@ -22,9 +25,23 @@ def ndx_groups(x, y):
 
     return " | ".join(index_list)
 
-print(ndx_groups(91, 101))
+
+print(ndx_groups(1, 9))
 
 
 
+
+
+
+def ndx_multigroups(x, z, o):
+    index_list = []
+    for i in range(0,z):
+        sequence = x+(i*o)
+        index_list.append(str(sequence))
+
+    return " | ".join(index_list)
+
+
+#print(ndx_multigroups(20, 311, 134))
 
 
